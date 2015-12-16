17a7965efed7558a84891acf58ecfbc8f28b318a
Oleg Shelajev <shelajev@gmail.com>
2015-12-16 14:32:31 +0200

    Created Android Studio project

---
+++ app/src/test/java/zeroturnaround/org/jrebel4androidgettingstarted/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package zeroturnaround.org.jrebel4androidgettingstarted;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
